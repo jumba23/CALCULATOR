e5f21f6f0e414b0e17aa1dcf84cd8fb1c79cdb20
Goran SD <g.cvetic23@gmail.com>
2021-04-24 15:19:40 -0700

    ready for submission

--- script.js
+++ script.js
@@ -1,106 +1,118 @@
-const numberButtons = document.querySelectorAll('button.number');
-const operationButtons = document.querySelectorAll('button.operation');
-const memoryButtons = document.querySelectorAll('memory');
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.querySelectorAll('.operation');
+const memoryButtons = document.querySelectorAll('.memory');
 const clearButton = document.querySelector('.clear');
 const equalSign = document.querySelector('.equal');
 const decimalNumber = document.querySelector('.decimal');
 
-
 let firstNumber = '';
 let secondNumber = '';
 let operationSelected = '';
-
+let memoryNumber = '';
 
 numberButtons.forEach(numberButton => numberButton.addEventListener('click', typeNumbers));
-operationButtons.forEach(operationButtons => operationButtons.addEventListener('click', operationAssigned));
+operationButtons.forEach(operationButton => operationButton.addEventListener('click', operationAssigned));
+memoryButtons.forEach(memoryButton => memoryButton.addEventListener('click', memoryCall));
 equalSign.addEventListener('click', operationAction);
 decimalNumber.addEventListener('click', decimalCheck);
+
+function typeNumbers(e) {
+    let numberSelection = e.target.value;
+    if (operationSelected !== '') {
+        secondNumber += numberSelection;
+        document.querySelector('input').value = '';
+        document.querySelector('input').value = secondNumber;
+    } else {
+        firstNumber += numberSelection;
+        document.querySelector('input').value = firstNumber;
+    }
+};
+
+function operationAssigned(e) {
+    if (firstNumber == '' && secondNumber == '') { }
+    else if (firstNumber !== '' && secondNumber == '') {
+        operationSelected = e.target.textContent;
+    } else if (firstNumber !== '' && secondNumber !== '') {
+        operationAction(operationSelected);
+        operationSelected = e.target.textContent;     
+    } else if (firstNumber == '' & secondNumber == '') {
+        firstNumber = document.querySelector('input').value;
+    }
+}
+
+function decimalCheck() {
+    if (operationSelected == '' && firstNumber.indexOf('.') == -1) {
+        firstNumber += '.';
+        document.querySelector('input').value = firstNumber;
+    } else if (secondNumber.indexOf('.') == -1 && operationSelected !== '') {
+        secondNumber += '.';
+        document.querySelector('input').value = secondNumber;
+    }
+}
+
+function memoryCall(e) {
+    memoryButton = e.target.value;
+    switch (memoryButton) {
+        case 'M+':
+            if (firstNumber !== '') {
+                memoryNumber = document.querySelector('input').value;
+            }
+            break;
+        case 'M-':
+            memoryNumber = '';
+            break;
+        case 'MR':
+            if (memoryNumber == '') { }
+            else if (operationSelected == '') {
+                document.querySelector('input').value = memoryNumber;
+                firstNumber = memoryNumber;
+            }
+            else if (secondNumber == '') {
+                secondNumber = memoryNumber;
+                document.querySelector('input').value = memoryNumber;
+            }
+            break;
+    }
+}
+
+function operationAction() {
+    if (secondNumber != '') {
+        switch (operationSelected) {
+            case 'X':
+                let product = firstNumber * secondNumber;
+                document.querySelector('input').value = product;
+                irstNumber = product;
+                operationSelected = '';
+                secondNumber = '';
+                break;
+            case '/':
+                let division = firstNumber / secondNumber;
+                document.querySelector('input').value = division;
+                firstNumber = division;
+                operationSelected = '';
+                secondNumber = '';
+                break;
+            case '+':
+                let addition = parseFloat(firstNumber) + parseFloat(secondNumber);
+                document.querySelector('input').value = addition;
+                firstNumber = addition;
+                operationSelected = '';
+                secondNumber = '';
+                break;
+            case '-':
+                let subtraction = firstNumber - secondNumber;
+                document.querySelector('input').value = subtraction;
+                firstNumber = subtraction;
+                operationSelected = '';
+                secondNumber = '';
+                break;
+        }
+    }
+}
 
 clearButton.addEventListener('click', () => {
     document.querySelector('input').value = '';
     firstNumber = '';
     secondNumber = '';
-    operationSelected ='';
-        });
-
-function decimalCheck(){
-     if(firstNumber.indexOf('.') == -1 && operationSelected == ''){
-            firstNumber += '.';
-            document.querySelector('input').value = firstNumber;
-            } else if(secondNumber.indexOf('.') == -1  && operationSelected !== ''){
-                    secondNumber += '.';
-                    document.querySelector('input').value = secondNumber;
-           }   
-}
-        
-function typeNumbers(e){
-        let numberSelection = e.target.value;
-        if(operationSelected !== ''){
-            secondNumber += numberSelection;
-            document.querySelector('input').value = '';
-            document.querySelector('input').value = secondNumber;
-            console.log('Second Number:' + secondNumber);
-            }else {
-                firstNumber += numberSelection;
-                document.querySelector('input').value = firstNumber;
-                console.log('First Number:' + firstNumber);
-                }          
-};
-
-function operationAssigned(e){
-        if (firstNumber == '' && secondNumber ==''){} 
-            else if (firstNumber !== '' && secondNumber == '' ){
-            operationSelected = e.target.textContent;
-            console.log('operation seclected: ' + operationSelected);
-            } else if(firstNumber !=='' && secondNumber !==''){
-                    operationAction(operationSelected);
-                    operationSelected = e.target.textContent;
-                    console.log('operation seclected: ' + operationSelected);
-                    }
-        }
-
-function operationAction(){
-    switch (operationSelected){
-        case 'X':
-            let product = firstNumber * secondNumber;
-            document.querySelector('input').value = product;
-            firstNumber = product;
-            secondNumber = '';
-            console.log('Result:' + product);
-            console.log('First Number:' + firstNumber);
-            console.log('Second Number:' + secondNumber);
-            break;
-        case '/':
-            let division = firstNumber / secondNumber;
-            document.querySelector('input').value = division;
-            firstNumber = division;
-            secondNumber = '';
-            console.log('Result:' + division);
-            console.log('First Number:' + firstNumber);
-            console.log('Second Number:' + secondNumber);
-            break;
-        case '+':
-            let addition = parseFloat(firstNumber) + parseFloat(secondNumber);
-            document.querySelector('input').value = addition;
-            firstNumber = addition;
-            secondNumber = '';
-            console.log('Result:' + addition);
-            console.log('First Number:' + firstNumber);
-            console.log('Second Number:' + secondNumber);
-            break;
-        case '-':
-            let subtraction = firstNumber - secondNumber;
-            document.querySelector('input').value = subtraction;
-            firstNumber = subtraction;
-            secondNumber = '';
-            console.log('Result:' + subtraction);
-            console.log('First Number:' + firstNumber);
-            console.log('Second Number:' + secondNumber);
-            break;
-    }   
-}
-
-//   console.log('Result:' + subtraction);
-//   console.log('First Number:' + firstNumber);
-//   console.log('Second Number:' + secondNumber);
-  
+    operationSelected = '';
+});
